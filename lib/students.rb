ee77367a9a16436fb41ff96d1dd229449d8a5903
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-05 14:12:29 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,4 +1,4 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
   SELECT * FROM students ORDER BY gpa DESC LIMIT 1;
